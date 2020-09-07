b9f7f89f883580404d01f3b2d80321b2752ea9de
fabrice.brito@terradue.com <fabrice.brito@terradue.com>
2020-09-07 14:20:48 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from io import open
+import os
+
+console_scripts = """
+[console_scripts]
+cwl2atom=cwl2atom.__init__:main
+"""
+
+print(find_packages(where='src'))
+
+setup(entry_points=console_scripts,
+      packages=find_packages(where='src'),
+      package_dir={'': 'src'})
+
---
+++ src/cwl2atom/__init__.py
@@ -0,0 +1,68 @@
+import logging
+import sys
+import yaml
+import os
+from .atom import Atom
+from lxml import etree
+import datetime
+import click
+
+logging.basicConfig(stream=sys.stderr, 
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%dT%H:%M:%S')
+
+@click.command()
+@click.argument('path')
+def main(path):
+    
+    with open(path, 'r') as stream:
+        try:
+            cwl = yaml.safe_load(stream)
+            
+            stream.seek(0)
+            
+            cwl_content = ''.join(stream.readlines())
+        except yaml.YAMLError as exc:
+            logging.error(exc)
+
+    for elem in cwl['$graph']:
+
+        if elem['class'] == 'Workflow':
+            workflow = elem
+
+    template = '''<?xml version="1.0"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+  <entry>
+    <title type="text"></title>
+    <summary type="html"></summary>
+    <date xmlns="http://purl.org/dc/elements/1.1/"></date>
+    <identifier xmlns="http://purl.org/dc/elements/1.1/"></identifier>
+  </entry>
+</feed>'''
+    
+    atom_template = Atom(etree.fromstring(template))
+    
+    atom_template.set_identifier(workflow['id'])
+    atom_template.set_title_text(workflow['label'])
+    atom_template.set_summary_text(workflow['doc'])
+
+    atom_template.set_dcdate(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M%SZ'))
+
+    offering = atom_template.create_offering(code='http://www.opengis.net/eoc/applicationContext/cwl',
+                                             content=cwl_content)
+
+    atom_template.add_offering(offering)
+    
+    sys.stdout.write(b'<?xml version="1.0" encoding="UTF-8"?>\n'.decode('utf-8'))
+    sys.stdout.write(etree.tostring(atom_template.root, pretty_print=True).decode('utf-8'))
+    
+    # if one wants to save to a file
+    #with open('pretty.xml', 'wb') as file: 
+        
+    #    file.write(b'<?xml version="1.0" encoding="UTF-8"?>\n')
+    #    file.write(etree.tostring(atom_template.root, pretty_print=True))
+        
+if __name__ == "__main__":
+    
+    main()
